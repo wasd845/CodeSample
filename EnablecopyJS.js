3dcd454dedb4782de68777e2818e6ae3f4eedd6b
wasd845 <845073947@qq.com>
2022-06-18 16:30:07 +0800

    Add files via upload

---
+++ EnablecopyJS.js
@@ -0,0 +1,9 @@
+window.removeEventListener("keyup", wpccpDisableCtrlActions, !1);
+window.removeEventListener("keydown", wpccpDisableCtrlActions, !1);
+window.removeEventListener("keyup", wpccpDisableKeys, !1);
+window.removeEventListener("keydown", wpccpDisableKeys, !1);
+window.removeEventListener("keydown", wpccpDisablePrintScreen, !1);
+window.removeEventListener("keyup", wpccpDisablePrintScreen, !1);
+window.document.ondragstart=0;
+window.document.oncontextmenu=0;
+window.document.body.onselectstart=0;
